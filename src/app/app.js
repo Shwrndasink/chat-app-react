a88388aa8595909675dce09a35ffd8c07f200f71
Marc White <mwhite@overstock.com>
2017-03-24 14:13:39 -0600

    minor update

--- src/app/app.js
+++ src/app/app.js
@@ -1,36 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import MessageList from './components/MessageList';
-
-let messages = [
-    {
-        id: 1,
-        timestamp: 1478859071000,
-        owner: 'Chuck',
-        text: 'Hello Mark, how is it going?'
-    },
-    {
-        id: 2,
-        timestamp: 1478859115000,
-        owner: 'Mark',
-        text: 'What do you want, Chuck?'
-    },
-    {
-        id: 3,
-        timestamp: 1478859131000,
-        owner: 'Chuck',
-        text: ':D'
-    },
-    {
-        id: 4,
-        timestamp: 1478859165000,
-        owner: 'Chuck',
-        text: 'I forgot you were not into small talk.'
-    }
-];
+import Chatbox from './components/Chatbox';
 
 ReactDOM.render(
-    <MessageList messages={messages} />,
+    <Chatbox />,
     document.querySelector( '.js-app' )
 );
